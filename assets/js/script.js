ed1274e4248d450b21bd15d91c949448a1223402
Tomas Perez <tomaspz@gmail.com>
2020-03-15 10:17:35 -0400

    fix weather icon display. Save element in local storage. Dynamically create a button for the city. Append the button to the aside

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,11 @@ $(function () {
     
         // grab the text inside the input field
         var searchCity = $("input").val();
-        // var cityLat =
-        // var cityLong = 
 
         // set the queryURL with the proper search query 
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=b5540c4c5b50c563c63c9a4c1e188656";
-        // var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=b5540c4c5b50c563c63c9a4c1e188656&lat=" + response.coord.lat + "&lon=" + response.coord.lon + "";
 
-        // create an ajax call to request the desired information for the city
+        // create a double ajax call to request the desired information for the city and the city uv index
 
         $.ajax({   
             url: queryURL,
@@ -27,9 +24,21 @@ $(function () {
                     method: "GET",
                     dataType: 'json',
                     success: function (response2) {
-                     console.log(response2);
-                     
-                     
+                        console.log(response2);
+                        // Create the card element to display in the body of the card
+                        var cardBodyEl = $("<h5 class='card-title' data-city='citi-name'>"  + response1.name +  " (<span> " + moment().format('l') + " </span>)<img id='icon' src='http://openweathermap.org/img/wn/"+ response1.weather[0].icon +".png' alt='current weather icon'/></h5><div class='weather-info'><p class='temp'>Temperature: " + Math.floor((response1.main.temp * 9/5) - 459.67) + " ºF</p><p class='hum'>Humidity: " + response1.main.humidity + "%</p><p class='wind'>Wind Speed: " + response1.wind.speed + " MPH</p><p class='uv'>UV Index: <span><button id='uvindex' type='button' class='btn text-white'>" + response2.value + "</button></span></p></div>");
+
+                        // Append element to the card
+                        $("#displaycity").append(cardBodyEl);
+
+                        // Save element in local storage
+                        localStorage.setItem(response1.name, cardBodyEl);
+
+                        // Create the city button on the left
+                        var cityButtonEl = $("<button type='button' class='btn btn-outline-info btn-block text-grey lighten-3'>"+ response1.name+"</button>");
+
+                        // Append the city button to the aside
+                        $(".btn-group-vertical").append(cityButtonEl);
                     }
                });
             }
@@ -37,13 +46,9 @@ $(function () {
         
     
 
-        //         // // Create the elements to display in the card
-        //         // var cardHeaderEl = $("<h5 class='card-title' data-city='citi-name'>"  + response.name +  " (<span> " + moment().format('l') + " </span>)<span><i>" + response.weather.icon + "</i></span></h5>");
+        
 
-        //         // var cardBodyEl = $("<div class='weather-info'><p class='temp'>Temperature: " + Math.floor((response.main.temp * 9/5) - 459.67) + " ºF</p><p class='hum'>Humidity: " + response.main.humidity + "%</p><p class='wind'>Wind Speed: " + response.wind.speed + " MPH</p><p class='uv>UV Index: <span><button id='uvindex' type='button' class='btn text-white'>" + ultraviolet + "</button></span></p></div>");
-
-        //         // // Append elements
-        //         // $(".card-body").append(cardHeaderEl, cardBodyEl);
+        
         //     });
         // }
 
